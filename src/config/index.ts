a1526f6ac684219edc8e50466bbabe619c8cc7bb
Shirloin <riccardo@binus.ac.id>
2024-09-08 23:09:17 +0700

    fix: docker compose

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 import { config } from "dotenv"
 
-config({ path: `.env` })
+config({ path: `.env.local` })
 export const CREDENTIALS = process.env.CREDENTIALS === 'true'
 export const { NODE_ENV, PORT, DB_HOST, DB_PORT, DB_NAME, SECRET_KEY, ORIGIN, } = process.env
