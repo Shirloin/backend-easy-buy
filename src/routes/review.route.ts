06c740270c05a9672ce8335c793b260097cd8473
Shirloin <riccardo@binus.ac.id>
2024-09-11 20:21:07 +0700

    fix: add router to create review

--- src/routes/review.route.ts
+++ src/routes/review.route.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
 import { Routes } from "../interfaces/auth.interface";
+import { authMiddleware } from "../middleware";
+import ReviewController from "../controllers/review.controller";
 
 export default class ReviewRoute implements Routes {
     public router = Router()
+    private reviewController = new ReviewController()
 
     constructor() {
         this.initializeRoutes()
     }
 
     private initializeRoutes() {
-        this.router
+        this.router.post("/review", authMiddleware, this.reviewController.createReview)
     }
 }
