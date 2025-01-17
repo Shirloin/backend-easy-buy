9ff967b346af6419c49877a6f8e29984760bae35
Shirloin <riccardo@binus.ac.id>
2025-01-17 10:50:58 +0700

    fix: start sh

--- start.prod.sh
+++ start.prod.sh
@@ -1,13 +1,3 @@
-check_mongodb_health() {
-  nc -z mongodb 27017
-}
-
-until check_mongodb_health; do
-  echo "Waiting for MongoDB to be healthy..."
-  sleep 2
-done
-
-echo "MongoDB is healthy. Running npm run seed..."
 
 npm run seed
 
